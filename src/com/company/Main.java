8b9d398d608d9e35554afedd4180cbe99a635be1
DejanStefanovski <dejanstefanovskister@gmail.com>
2021-03-09 10:23:22 +0100

    Izmena vo odzemanjeto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
         AdvanceCalculator calc1 = new AdvanceCalculator();
         int add = calc1.add(1, 5);
         System.out.println("1+5 = " +add);
-        int subtract = calc1.subtract(5, 3);
-        System.out.println("5-3 = " +subtract);
+        int subtract = calc1.subtract(5, 2);
+        System.out.println("5-2 = " +subtract);
 
         double multiply = calc1.multiply(4 , 5);
         System.out.println("4*5 = " +multiply);
